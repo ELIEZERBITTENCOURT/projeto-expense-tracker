8325ddffd4fec1166a1d028c96e016b9a56d71a4
Eliezer Bittencourt <likobittencourt@gmail.com>
2021-10-06 19:37:16 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 6), category: 'food', title: 'Ifood', value: 32.12 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Uber Eats', value: 28 },
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Condomínio', value: 2300 },
+    { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário ACME', value: 4500 },
+];
